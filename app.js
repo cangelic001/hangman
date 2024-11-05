8b9f0a01b0491739e5099ceb09ab8e92b34f0f2f
cangelic001 <ica.christina992@gmail.com>
2024-11-05 18:41:01 +0800

    commit

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const words = ["cow", "pig", "cat", "elk", "fox", "rat", "bee", "ant", "owl", "cod"];
+
+const maxWrongGuesses = 10;
+
+function startGame() {
+    let selectedWord = words[0];   
+};
+
+function 
+
+
